635591fc4f13c9fa42547e93d13660d5f033b2ad
bassilevss <nbasi13@freeuni.edu.ge>
2021-01-19 21:51:13 +0400

    Change directory structure and add sample model

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
 FROM ubuntu:20.04
 
-
 RUN apt-get update
 RUN apt-get install python3-dev python3-pip -y
 
-COPY . /app
+COPY ./api /api/api
 COPY requirements.txt /requirements.txt
 
-ENV PYTHONPATH=/app
+ENV PYTHONPATH=/aoi
 
 RUN pip3 install -r requirements.txt
-WORKDIR /app
+WORKDIR /api
 
 EXPOSE 8000
 
 ENTRYPOINT ["uvicorn"]
-CMD ["main:app", "--host", "0.0.0.0"]
+CMD ["api.main:app", "--host", "0.0.0.0"]
---
+++ api/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, validator
+
+from .alexaDL.model import Model, get_model, n_features
+from typing import List
+
+
+class PredictRequest(BaseModel):
+    data: List[str]
+
+    @validator("data")
+    def check_dimensionality(self, words):
+        if len(words) != n_features:
+            raise ValueError(f"Input must contain {n_features} words")
+
+        return words
+
+
+class PredictResponse(BaseModel):
+    data: str
+
+
+app = FastAPI()
+
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(x: PredictRequest, model: Model = Depends(get_model)):
+    x = x.data
+    y_pred = model.predict(x)
+    result = PredictResponse(data=y_pred)
+
+    return result
